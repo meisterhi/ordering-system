4f82cc3d2dc693e64993298948094d6d1e628caa
Sven Graupner <sgraupner@bht-berlin.de>
2025-11-25 22:37:39 +0100

    add src/main, src/tests, src/resources

---
+++ src/tests/application/Application_0_always_pass_Tests.java
@@ -0,0 +1,36 @@
+package application;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Test class to test the JUnit test setup. @Test methods of this class
+ * always pass. If JUnit is setup properly, tests should run in the IDE
+ * and in the terminal.
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class Application_0_always_pass_Tests {
+
+    /**
+     * First @Test method (always passes).
+     */
+    @Test
+    @Order(001)
+    void test_001_always_pass() {
+        int expected = 10;
+        int actual = 10;
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Second @Test method (always passes).
+     */
+    @Test
+    @Order(002)
+    void test_002_always_pass() {
+        int expected = 10;
+        int actual = 10;
+        assertEquals(expected, actual);
+    }
+}
